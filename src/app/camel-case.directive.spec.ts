9ba970a959591d2942109a40e1963f2994ff3248
Jelly <a.krzychu@gmail.com>
2020-09-29 07:39:21 +0200

    [Feature] Preparing test Angular i18n project;

---
+++ src/app/camel-case.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CamelCaseDirective } from './camel-case.directive';
+
+describe('CamelCaseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CamelCaseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
