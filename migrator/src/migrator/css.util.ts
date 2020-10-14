9131154c20bcb90eac4af0462d6099ec9b9b54e8
Krzysztof Antczak <krantczak@pzu.pl>
2020-10-14 20:53:49 +0200

    [Bugfix] Check if css class has host and ng-deep selectors;

--- migrator/src/migrator/css.util.ts
+++ migrator/src/migrator/css.util.ts
@@ -1,23 +1,17 @@
 const {parse, stringify} = require('scss-parser');
 const createQueryWrapper = require('query-ast');
-import jsBeautify = require('js-beautify');
+
 
 export class CssUtil {
 
   public static encapsulateClasses(styleFileContent: string, classesToEncapsulate): string {
-    let astCss: Node;
-    try {
-      astCss = parse(this.addMissingSemicolons(styleFileContent));
-    } catch (e) {
-      const x = jsBeautify.css(styleFileContent);
-      console.log(e);
-    }
+    const astCss = parse(this.addMissingSemicolons(styleFileContent));
     const $ = createQueryWrapper(astCss);
 
     classesToEncapsulate.forEach(className => {
-      const selector = $('class').filter(e => e.node.value.some(k => k.value === className));
-      if (selector.nodes.length > 0) {
-        const selectorElements = selector.nodes[0].parent.node.value;
+      const parentSelector = this.getSelectorByClassName($, className);
+      if (!!parentSelector && !this.hasHostAndDeepSelectors(parentSelector)) {
+        const selectorElements = parentSelector.nodes[0].node.value;
         selectorElements.unshift(this.createSelector('space', ' '));
         selectorElements.unshift(this.createSelector('pseudo_class', ':ng-deep'));
         selectorElements.unshift(this.createSelector('space', ' '));
@@ -26,6 +20,17 @@ export class CssUtil {
     });
 
     return stringify(astCss);
+  }
+
+  private static getSelectorByClassName($, className: string) {
+    return $((n) => n.node.value === className)
+      .closest('selector');
+  }
+
+  private static hasHostAndDeepSelectors($): boolean {
+    return $
+      .has(e => e.node.value === 'host')
+      .has(e => e.node.value === 'ng-deep').nodes.length > 0;
   }
 
   private static createSelector = (type: string, value: string) => ({type, value: [{type: 'identifier', value}]});
