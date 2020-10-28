2f9c161515dae5926298a47560995ff00e344b36
Jelly <a.krzychu@gmail.com>
2020-10-28 19:15:25 +0100

    [Refactor] Some clean up;

--- src/app/camel-case.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CamelCaseDirective } from './camel-case.directive';
-
-describe('CamelCaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CamelCaseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
