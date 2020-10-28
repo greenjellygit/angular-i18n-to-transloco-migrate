9d92b045675c49028c4478388138f428c47fd893
Krzysztof Antczak <krantczak@pzu.pl>
2020-10-28 19:15:25 +0100

    [Refactor] Some clean up;

--- migrator/src/migrator/array.utils.ts
+++ migrator/src/migrator/array.utils.ts
@@ -1,17 +1,7 @@
 export class ArrayUtils {
 
-  const;
-
   public static isNotEmpty(array: any[]): boolean {
     return !!array && Array.isArray(array) && array.length > 0;
-  }
-
-  public static removeDuplicates(array: any[], byPropertyName): any[] {
-    return array.filter((e, index, self) =>
-      index === self.findIndex((t) => (
-        t[byPropertyName] === e[byPropertyName]
-      ))
-    );
   }
 
   public static groupByKey(list, key, omitKey = false) {
