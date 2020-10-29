ee8d942a079d2e1f1e23ab6fc274d3325f5a8802
Jelly <a.krzychu@gmail.com>
2020-10-29 17:35:02 +0100

    [Refactor] Rename ReplacePlaceholderStrategy to FillPlaceholderStrategy;

--- migrator/src/migrator/fill-placeholder/base/fill-placeholder.strategy.ts
+++ migrator/src/migrator/fill-placeholder/base/fill-placeholder.strategy.ts
@@ -2,11 +2,11 @@ import {Message} from '@angular/compiler/src/i18n/i18n_ast';
 import {ParsedTranslationBundle} from '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/translation_parser';
 import {ParsedPlaceholder, ParsedPlaceholdersMap, TranslationKey} from '../../index';
 
-export abstract class ReplacePlaceholderStrategy {
+export abstract class FillPlaceholderStrategy {
 
   constructor(protected placeholder: string) { }
 
-  abstract replace(text: string, parsedPlaceholder: ParsedPlaceholder, message?: Message,
-                   placeholdersMap?: ParsedPlaceholdersMap, localeBundle?: ParsedTranslationBundle, translationKey?: TranslationKey): string;
+  abstract fill(text: string, parsedPlaceholder: ParsedPlaceholder, message?: Message,
+                placeholdersMap?: ParsedPlaceholdersMap, localeBundle?: ParsedTranslationBundle, translationKey?: TranslationKey): string;
 
 }
--- migrator/src/migrator/fill-placeholder/fill-html-placeholder.ts
+++ migrator/src/migrator/fill-placeholder/fill-html-placeholder.ts
@@ -1,10 +1,10 @@
 import {Message} from '@angular/compiler/src/i18n/i18n_ast';
 import {ParsedPlaceholder} from '../index';
-import {ReplacePlaceholderStrategy} from './base/replace-placeholder.strategy';
+import {FillPlaceholderStrategy} from './base/fill-placeholder.strategy';
 
-export class ReplaceDefaultPlaceholder extends ReplacePlaceholderStrategy {
+export class FillHtmlPlaceholder extends FillPlaceholderStrategy {
 
-  replace(text: string, parsedPlaceholder: ParsedPlaceholder, message: Message): string {
+  fill(text: string, parsedPlaceholder: ParsedPlaceholder, message: Message): string {
     return text
       .replace(`{$${this.placeholder}}`, message.placeholders[this.placeholder])
       .replace(new RegExp(`{${this.placeholder}}`, 'g'), message.placeholders[this.placeholder]);
